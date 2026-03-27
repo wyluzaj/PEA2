68a2cd6686f554790cae7b37429ec7a1540e6c06
Lukerya <l.6808561@gmail.com>
2026-03-27 08:44:00 +0100

    kod do v ost

--- kod/bnb/ub.h
+++ kod/bnb/ub.h
@@ -1,23 +1,4 @@
-//#ifndef PEA2_UB_H
-//#define PEA2_UB_H
-//#pragma once
-//
-//#include <chrono>
-//#include <vector>
-//
-//#include "../common/tsp_instance.h"
-//
-//struct UpperBoundResult {
-//    int cost = -1;
-//    std::vector<int> path;
-//    bool finished = false;
-//    bool timeoutReached = false;
-//};
-//
-//UpperBoundResult computeInitialUpperBoundNN(
-//        const TSPInstance& instance
-//);
-//#endif //PEA2_UB_H
+
 #ifndef PEA2_UB_H
 #define PEA2_UB_H
 #pragma once
